daecbb480120621fb9b43cca912b3684d4e7ec15
Premwut Klaychim <premwutklaychim@Premwuts-MacBook-Pro.local>
2024-12-02 15:17:37 +0700

    feat: initialize controllers & services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TopicsController } from './controllers/topics.controller';
+import { CommentsController } from './controllers/comments.controller';
+import { TopicsService } from './services/topics.service';
+import { CommentsService } from './services/comments.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, TopicsController, CommentsController],
+  providers: [AppService, TopicsService, CommentsService],
 })
 export class AppModule {}
